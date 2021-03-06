3000a8e1817758da1cc47c0b4b2a5c1070dcda0d
Patrick Pang <patrick.pang@vu.edu.au>
2021-03-06 08:21:53 +0000

    Remove unused imports.

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,10 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
+    "sap/ui/core/mvc/Controller"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller, MessageBox) {
+	function (Controller) {
 		"use strict";
 
 		return Controller.extend("flight.controller.View1", {
